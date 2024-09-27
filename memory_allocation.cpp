20196aefe377f09b11ef9122e76324d2a7ce4a11
anthony-vo <s3951749@student.rmit.edu.au>
2024-09-27 20:33:52 +1000

    Function to determine the fixed partition

--- memory_allocation.cpp
+++ memory_allocation.cpp
@@ -1,13 +1,29 @@
 #include <iostream>
 #include <list> // For linked list implementation
-#include <cstdlib> // For sbrk()
+#include <cstdlib> // For exit()
+#include <unistd.h> // for sbrk()
+#include <array>
 
 // Struct to represent a memory chunk
 struct MemoryChunk {
-    std::size_t size; // Size of the chunk
+    std::size_t requested; // Total size of the partition
+    std::size_t used; // Actual size being used
     void* address; // Address of the chunk
 };
 
 // Lists to hold free and allocated memory chunks
-std::list<MemoryChunk> free_list;
-std::list<MemoryChunk> allocated_list;
+std::list<MemoryChunk> free;
+std::list<MemoryChunk> allocated;
+
+// Fixed partition sizes as mentioned in the specification
+const std::array<std::size_t, 5> chunk_sizes = {32, 64, 128, 256, 512};
+
+// Function to find the appropriate chunk size for the allocating memory
+std::size_t find_size(std::size_t requested_chunk_size) {
+  for (std::size_t size : chunk_sizes) {
+    if (requested_chunk_size <= size) {
+      return size;
+    }
+  }
+  return 0;
+}
