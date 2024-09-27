36632f0ac05551ca416d3699dae3512b580505a9
anthony-vo <s3951749@student.rmit.edu.au>
2024-09-27 20:44:50 +1000

    alloc and dealloc

--- memory_allocation.cpp
+++ memory_allocation.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
-#include <list> // For linked list implementation
 #include <cstdlib> // For exit()
 #include <unistd.h> // for sbrk()
+#include <list>
 #include <array>
 
-// Struct to represent a memory chunk
+// Define the struct for allocation
 struct MemoryChunk {
-    std::size_t requested; // Total size of the partition
-    std::size_t used; // Actual size being used
-    void* address; // Address of the chunk
+  std::size_t requested; // Total size of the partition
+  std::size_t used; // Actual size being used
+  void* space; // Pointer to the memory space of the allocation
 };
 
-// Lists to hold free and allocated memory chunks
-std::list<MemoryChunk> free;
+// Global list for allocated and free memory
 std::list<MemoryChunk> allocated;
+std::list<MemoryChunk> free;
 
 // Fixed partition sizes as mentioned in the specification
 const std::array<std::size_t, 5> chunk_sizes = {32, 64, 128, 256, 512};
@@ -27,3 +27,63 @@ std::size_t find_size(std::size_t requested_chunk_size) {
   }
   return 0;
 }
+
+// Memory allocation
+void* alloc(std::size_t chunk_size) {
+  std::size_t actual;
+
+  // If the requested chunk size is bigger than the partition, assign immediately
+  if (chunk_size > 512) {
+    actual = chunk_size;  // No need for partition size, use the requested size
+  } else {
+    actual = find_size(chunk_size);
+    if (actual == 0) {
+    std::cerr << "Invalid chunk size!" << std::endl;
+    return nullptr;
+    }
+  }
+
+  // Scan through the free list for a free chunk (first fit strategy?)
+  for (auto iterator = free.begin(); iterator != free.end(); ++iterator) {
+    if (iterator->requested >= actual) {
+      // Found a chunk to allocate the memory, move the chunk to allocated list
+      MemoryChunk alloc = *iterator;
+      alloc.used = chunk_size; // Keep track the used size for printing purposes
+      free.erase(iterator); // delete the free space
+      allocated.push_back(alloc);
+      return alloc.space;
+    }
+  }
+
+  // If no suitable fixed-size chunk is found, make a OS call to request for more memory
+  void* request_space = sbrk(actual);
+  if (request_space == (void*)-1) {
+    std::cerr << "There is an error in memory allocation!" << std::endl;
+    return nullptr;
+  }
+
+  // Create a new allocation and add it to the allocated
+  MemoryChunk new_allocation = {actual, chunk_size, request_space};
+  allocated.push_back(new_allocation);
+  return request_space;
+}
+
+// Memory de-allocation
+void dealloc(void* chunk) {
+  // Find the chunk in the allocated linked list
+  for (auto iterator = allocated.begin(); iterator != allocated.end(); ++iterator) {
+    if (iterator->space == chunk) {
+      // Move the chunk back to the free list
+      MemoryChunk deallocated = *iterator;
+      allocated.erase(iterator);  // Remove from the list
+      free.push_back(deallocated);  // Add to the end
+      return;
+    }
+  }
+
+  // If there is an attempt to de-allocate a non-existent chunk, terminate the program
+  std::cerr << "Attempt to free un-allocated/non-existing memory!" << std::endl;
+  exit(EXIT_FAILURE);
+}
+
+// Function to print the status
